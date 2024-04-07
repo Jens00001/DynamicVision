498e1f318fc43fdc7f7245a3653af74068c57fec
Daniel Philippi <philippidaniel98@gmail.com>
2024-04-08 00:12:47 +0200

    added a numerical simulation method for the equation of motion (including changes of previous methods to make a simulation possible) and added documentation for two methods

--- Modeling/lagrange.py
+++ Modeling/lagrange.py
@@ -1,62 +1,90 @@
-from sympy import sin, cos, Function
 import sympy as sp
-import sys
-
+from sympy import lambdify
+from scipy.integrate import solve_ivp
+from numpy import linspace
 class Lagrange:
     """
     Class for calculating the equation of motion using Lagrange (symbolic)
 
-    :param q: generalized coordinate of the system
-    :type q: sympy.Function (q depends on time q(t))
+    :param q: generalized coordinate of the system and the derivatives that appear in the equations
+    :type q: list of sympy.Function (q depends on time q(t))
     :param t: time variable
     :type t: sympy.Symbol
     :param T: kinetic energy
     :type T: sympy.Add or sympy.Mul
     :param U: potential energy
     :type U: sympy.Add or sympy.Mul
+    :param F: external force (1-dim linear force)
+    :type F: sympy.Add, sympy.Mul or int if the force is constant
     :param k: kinematic (geometric) equation
-    :type k: sympy.Add, sympy.Mul or int if there is no kinematic equation
+    :type k: sympy.Add, sympy.Mul or int if the kinematic equation is constant
     :raises Lagrange.InvalidCoordError: If the generalized coordinate is invalid
     :raises Lagrange.InvalidEnergyError: If the energies is invalid
     :raises Lagrange.InvalidKinematicError: If the kinematic equation is invalid
-    :return: symbolic equation of motion
-    :rtype: sympy.Add (symbolic sum --> equation of motion)
     """
 
-    def __init__(self, q, t, T, U, k):
-        self.T = T      # kinetic energy
-        self.U = U      # potential energy
-        self.q = q      # generalized coordinate
-        self.k = k      # kinetic
-        self.t = t      # time variable
+    def __init__(self, q, t, T, U, F, k):
+        self.T = T
+        self.U = U
+        self.q = q
+        self.k = k
+        self.t = t
+        self.F = F
 
     def __repr__(self):
         return str(list(self.__dict__.items()))
 
     def check_attributes(self):
-        assert isinstance(self.T, (sp.Mul, sp.Add))          # kinetic energy is either a single term or a sum
-        assert isinstance(self.U, (sp.Mul, sp.Add))          # potential energy is either a single term or a sum
-        assert isinstance(self.q, sp.Function)               # generalized coordinate is a function of time
-        assert isinstance(self.k, (sp.Mul, sp.Add, int))     # kinematic equation is either a single term, a sum or 0 (int)
+        assert isinstance(self.T, (sp.Mul, sp.Add))
+        assert isinstance(self.U, (sp.Mul, sp.Add))
+        assert isinstance(self.q, list)
+        assert isinstance(self.k, (sp.Mul, sp.Add, int))
 
     def lagrange_equation(self):
+        # This function calculates the lagrangian
         L = self.T - self.U
         L = L.expand()
-        L = sp.trigsimp(L)
-        self.L = L
-        return self.L
+        return sp.trigsimp(L)
 
-    def equation_of_motion(self):
-        L_dq = self.L.diff(self.q)
-        L_dqd = self.L.diff(self.q.diff(self.t))
+    def lagrangian(self):
+        """
+        Function for computing the analytical Lagrange equations
+
+        :return: symbolic equation of motion
+        :rtype: sympy.Add (symbolic sum --> equation of motion)
+        """
+        L = self.lagrange_equation()
+        # This function calculates the equation of motion
+        L_dq = L.diff(self.q[0])
+        L_dqd = L.diff(self.q[1])
         DL_dqd = L_dqd.diff(self.t)
-        eq = DL_dqd - L_dq
+        eq = DL_dqd - L_dq - self.F
         eq = eq.expand()
         eq = sp.trigsimp(eq)
-        self.eq = eq
-        return self.eq
+        eq = sp.Eq(eq, 0)
+        return sp.solve(eq, self.q[0].diff(self.t, 2))
 
+    def simulate(self, init_cond, t_span, num_points=1001):
+        """
+        Function for computing the numerical equation of motion for simulation
+
+        :return: list of first order differential equations
+        :rtype: list
+        """
+        # get Lagrangian
+        L = self.lagrangian()[0]
+
+        # Convert symbolic function to a callable function
+        rhs_func = lambdify((self.t, self.q[0]), L.subs(self.q[0].diff(self.t), self.q[1]))
+
+        # function to convert second order system to two first order system
+        def second_to_one(t, y):
+            return [y[1], rhs_func(t, y[0])]
+
+        return solve_ivp(second_to_one, t_span, init_cond, t_eval=linspace(t_span[0], t_span[1], num_points))
 
     # TODO:
-    # implement translatoric external force
-    # implement multiple generalized coordinates
+    # extend check_attribute method
+    # implement more than only second order systems
+    # implement the possibility for kinematic equations --> e.x. pre-strech of spring
+    # insert documentation for every method
--- Modeling/main_test.py
+++ Modeling/main_test.py
@@ -1,6 +1,7 @@
-from sympy import Function, pprint, solve
+from sympy import Function, pprint
 import sympy as sp
 import lagrange
+from matplotlib import pyplot as plt
 
 def preview(expr, **kwargs):
     """
@@ -14,31 +15,41 @@ def preview(expr, **kwargs):
     plt.axis("off")
     plt.show()
 
-params = sp.symbols("m, g, k")
-m, g, k = params
+params = sp.symbols("m, g, k, d")
+m, g, k, d = params
 
-# task 2
 t = sp.Symbol("t")  # create the symbol for the time
 xt = Function("x")(t)  # x(t)
-
 xdt = xt.diff(t)
 xddt = xt.diff(t, 2)
+q = [xt, xdt]
 
 T = 1/2 * m * xdt**2
 U = 1/2 * k * xt**2 + m * g * xt
 
-L1 = lagrange.Lagrange(xt, t, T, U,0)
+x = sp.Symbol("x")  # create the symbol for the time
+T = T.subs([(m, 20), (g, 9.81), (k, 10)])
+U = U.subs([(m, 20), (g, 9.81), (k, 10)])
 
-L_eq = L1.lagrange_equation()
-Eq = L1.equation_of_motion()
+L1 = lagrange.Lagrange(q, t, T, U, 0, 0)
 
-res = solve(Eq, xddt)
-xdd_expr = res[0]
+L_eq = L1.lagrangian()[0]
+pprint(L_eq)
+# plot equation
+xdd_expr = L_eq
 x, xd, xdd = sp.symbols("x, xd, xdd")
 rplmts = [(xddt, xdd), (xdt, xd), (xt, x)]
 Eq1a = sp.Eq(xdd, xdd_expr.subs(rplmts))
-
 # provide LaTeX notation for the symbols
 sn_dict = {xd: r"\dot{x}", xdd: r"\ddot{x}"}
 
 preview(Eq1a, symbol_names=sn_dict)
+
+# simulation and plot result
+x0 = [0, 0]
+t_span = (0, 100)
+sol = L1.simulate(x0, t_span)
+
+plt.plot(sol.t, sol.y[0])
+plt.show()
+
