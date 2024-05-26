46c7461f29789c488657f206484d2bfd18527c90
xiCentauri <s8nssche@uni-saarland.de>
2024-05-26 12:53:35 +0200

    Added some panels to adjust the Element

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -40,7 +40,7 @@ class StartMenu(wx.Panel):
         # Calculate the positions for the buttons 
         panel_size = self.GetSize()
         x_pos_btn = (panel_size[0]-button_size[0])//2
-        y_pos_btn = (panel_size[1]-button_size[1]*3)//2
+        y_pos_btn = (panel_size[1]-button_size[1]*4)//2
 
         # Set the position of the Buttons in the Center of the Panel 
         self.button_create_model.SetPosition((x_pos_btn, y_pos_btn))
@@ -164,14 +164,123 @@ class MassElement(wx.Panel):
     def __init__(self,parent):
         wx.Panel.__init__(self,parent,size=(400,200))
         self.SetBackgroundColour(wx.Colour(255,255,255))
-        text = wx.StaticText(self, label="Mass", pos=(50, 50))  # Add a static text to the panel
+        #text = wx.StaticText(self, label="Mass", pos=(50, 50))  # Add a static text to the panel
+
+        # Define Button Size  
+        button_size_element = (100,25)
+
+        # Create Button to Switch to the Mass panel
+        self.button_mass_point = wx.Button(self,label="Mass Point",size=button_size_element)
+        self.Bind(wx.EVT_BUTTON,parent.on_mass_point,self.button_mass_point)
+
+        # Create Button to Switch to Spring Panel
+        self.button_steady_body = wx.Button(self,label="Steady Body",size = button_size_element)
+        self.Bind(wx.EVT_BUTTON,parent.on_mass_steady,self.button_steady_body)
+
+        # Calculate the positions for the buttons 
+        panel_size = self.GetSize()
+        x_pos_btn = (panel_size[0]-button_size_element[0])//2
+        y_pos_btn = (panel_size[1]-button_size_element[1]*3)//2
+
+        # Set the position of the Buttons in the Center of the Panel 
+        self.button_mass_point.SetPosition((x_pos_btn, y_pos_btn))
+        self.button_steady_body.SetPosition((x_pos_btn, y_pos_btn + button_size_element[1]))
+
+class MassPoint(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self,parent,size=(400,200))
+        self.SetBackgroundColour(wx.Colour(255,255,255))
+        text = wx.StaticText(self, label="MassPoint", pos=(50, 50))  # Add a static text to the panel
+        self.mass_label = wx.StaticText(self,label = "What is the mass of the Mass Point:",pos=(10,10))
+        self.mass = wx.TextCtrl(self, pos=(10, 30), size=(200, -1))
+        self.input_mass = None
+
+class SteadyBody(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self,parent,size=(400,200))
+        self.SetBackgroundColour(wx.Colour(255,255,255))
+        text = wx.StaticText(self, label="SteadyBody", pos=(50, 50))  # Add a static text to the panel
+
+        self.density_label = wx.StaticText(self.panel,label = "What is the density of the Steady Body:",pos=(10,10))
+        self.density = wx.TextCtrl(self, pos=(10, 30), size=(200, -1))
+        self.input_density = None
+
+        self.length_label = wx.StaticText(self.panel,label = "What is the length of the Steady Body:",pos=(10,10))
+        self.length = wx.TextCtrl(self, pos=(10, 30), size=(200, -1))
+        self.input_length = None 
+
+        self.height_label = wx.StaticText(self.panel,label = "What is the height of the Steady Body:",pos=(10,10))
+        self.height = wx.TextCtrl(self, pos=(10, 30), size=(200, -1))
+        self.input_height = None
+
+        self.width_label = wx.StaticText(self.panel,label = "What is the width of the Steady Body:",pos=(10,10))
+        self.width = wx.TextCtrl(self, pos=(10, 30), size=(200, -1))
+        self.input_width = None
+
 
 # Create the Panel "Spring"
 class SpringElement(wx.Panel):
     def __init__(self,parent):
         wx.Panel.__init__(self,parent,size=(400,200))
         self.SetBackgroundColour(wx.Colour(255,255,255))
-        text = wx.StaticText(self, label="Spring", pos=(50, 50))  # Add a static text to the panel
+        #text = wx.StaticText(self, label="Spring", pos=(50, 50))  # Add a static text to the panel
+
+        # Define Button Size  
+        button_size_element = (100,25)
+
+        # Create Button to Switch to the Single Spring
+        self.button_spring_single = wx.Button(self,label="Single Spring",size=button_size_element)
+        self.Bind(wx.EVT_BUTTON,parent.on_spring_single,self.button_spring_single)
+
+        # Create Button to Switch to Parallel Springs
+        self.button_spring_parallel= wx.Button(self,label="Several Paralell Springs",size = button_size_element)
+        self.Bind(wx.EVT_BUTTON,parent.on_spring_parallel,self.button_spring_parallel)
+
+        # Create Button to Switch to Series Springs
+        self.button_spring_series = wx.Button(self,label="Several Springs in Series",size = button_size_element)
+        self.Bind(wx.EVT_BUTTON,parent.on_spring_series,self.button_spring_series)
+
+        # Calculate the positions for the buttons 
+        panel_size = self.GetSize()
+        x_pos_btn = (panel_size[0]-button_size_element[0])//2
+        y_pos_btn = (panel_size[1]-button_size_element[1]*3)//2
+
+        # Set the position of the Buttons in the Center of the Panel 
+        self.button_spring_single.SetPosition((x_pos_btn, y_pos_btn))
+        self.button_spring_parallel.SetPosition((x_pos_btn, y_pos_btn + button_size_element[1]))
+        self.button_spring_series.SetPosition((x_pos_btn, y_pos_btn + 2*button_size_element[1]))
+
+class SingleSpring(wx.Panel):
+    def __init__(self,parent):
+        wx.Panel.__init__(self,parent,size=(400,200))
+        self.SetBackgroundColour(wx.Colour(255,255,255))
+        text = wx.StaticText(self, label="Single Spring", pos=(50, 50))  # Add a static text to the panel
+
+        self.length_spring_label = wx.StaticText(self.panel,label = "What is the length of the spring:",pos=(10,10))
+        self.length_spring = wx.TextCtrl(self, pos=(10, 30), size=(200, -1))
+        self.input_length_spring = None
+
+        self.stiffness_spring_label = wx.StaticText(self.panel,label = "What is the stiffness of the spring:",pos=(10,10))
+        self.stiffness_spring = wx.TextCtrl(self, pos=(10, 30), size=(200, -1))
+        self.input_stiffness_spring = None
+
+class ParallelSpring(wx.Panel):
+    def __init__(self,parent):
+        wx.Panel.__init__(self,parent,size=(400,200))
+        self.SetBackgroundColour(wx.Colour(255,255,255))
+        text = wx.StaticText(self, label="Sveral parallel springs", pos=(50, 50))  # Add a static text to the panel
+
+        self.length_spring_label = wx.StaticText(self.panel,label = "What is the length of the spring:",pos=(10,10))
+        self.length_spring = wx.TextCtrl(self, pos=(10, 30), size=(200, -1))
+        self.input_length_spring = None
+
+class SeriesSpring(wx.Panel):
+    def __init__(self,parent):
+        wx.Panel.__init__(self,parent,size=(400,200))
+        self.SetBackgroundColour(wx.Colour(255,255,255))
+        text = wx.StaticText(self, label="Several springs in series", pos=(50, 50))  # Add a static text to the panel
+
+
 
 # Create The Frame for the Create Element 
 class CreateElement(wx.Frame):
@@ -181,10 +290,21 @@ class CreateElement(wx.Frame):
         self.choose_element = ChooseElement(self)
         self.mass_element = MassElement(self)
         self.spring_element = SpringElement(self)
+        self.mass_point = MassPoint(self)
+        self.mass_steady_body = SteadyBody(self)
+        self.spring_single = SingleSpring(self)
+        self.spring_parallel = ParallelSpring(self)
+        self.spring_series = SeriesSpring(self)
 
         self.choose_element.Show()
         self.mass_element.Hide()
         self.spring_element.Hide()
+        self.mass_point.Hide()
+        self.mass_steady_body.Hide() 
+        self.spring_single.Hide()
+        self.spring_parallel.Hide() 
+        self.spring_series.Hide() 
+        
 
     # Method to switch to Create Mass 
     def on_mass(self,event):
@@ -192,10 +312,40 @@ class CreateElement(wx.Frame):
         self.mass_element.Show() 
         self.Layout()
 
+    # Method to switch to Mass Point 
+    def on_mass_point(self,event):
+        self.mass_element.Hide() 
+        self.mass_point.Show()
+        self.Layout()
+
+    # Method to switch to Steady Body
+    def on_mass_steady(self,event):
+        self.mass_element.Hide() 
+        self.mass_steady_body.Show()
+        self.Layout()     
+
     # Method to switch to Create Spring 
     def on_spring(self,event):
         self.choose_element.Hide()
         self.spring_element.Show()
+        self.Layout()
+
+        # Method to switch to Single Springs 
+    def on_spring_single(self,event):
+        self.spring_element.Hide()
+        self.spring_single.Show()
+        self.Layout()
+
+        # Method to switch to Parallel Springs
+    def on_spring_parallel(self,event):
+        self.spring_element.Hide()
+        self.spring_parallel.Show()
+        self.Layout()
+
+        # Method to switch to Springs in Series
+    def on_spring_series(self,event):
+        self.spring_element.Hide()
+        self.spring_series.Show()
         self.Layout()
 
 # Create the Main Frame 
